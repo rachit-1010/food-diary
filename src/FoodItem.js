6bd316f43add46075dd86075a50fd881408de509
Rachit Shah <shahrm@uchicago.edu>
2023-09-22 13:18:34 -0500

    BUG FIX : 12:00 PM showed as 00:00 PM

--- src/FoodItem.js
+++ src/FoodItem.js
@@ -1,5 +1,8 @@
 export default function FoodItem(props) {
 	const { index, foodItem } = props
+	let hours = foodItem.timeStamp.getHours()%12;
+	if (hours === 0) hours = 12;
+
 	return (
 		<div key={index}>
 			<div className={'health-status ' + (foodItem.isHealthy ? 'healthy' : 'unhealthy')}>
@@ -7,7 +10,7 @@ export default function FoodItem(props) {
 					{foodItem.food} 
 				</div>
 				<div className="timeStamp">
-					{ `${foodItem.timeStamp.getHours()%12}:${(foodItem.timeStamp.getMinutes()<10 ? '0'+foodItem.timeStamp.getMinutes() : foodItem.timeStamp.getMinutes())} ` + (foodItem.timeStamp.getHours()/12<1 ? 'AM' : 'PM') }
+					{ `${hours}:${(foodItem.timeStamp.getMinutes()<10 ? '0'+foodItem.timeStamp.getMinutes() : foodItem.timeStamp.getMinutes())} ` + (foodItem.timeStamp.getHours()/12<1 ? 'AM' : 'PM') }
 				</div>
 			</div>
 		</div>
